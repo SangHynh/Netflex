6beae03f586a3bf370b4de9e8847ad9dbda31301
SangHynh <huynhvansang.020102@gmail.com>
2024-07-31 18:15:41 +0700

    create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className='p-4 bg-black flex items-center justify-between opacity-85'>
+      <div className='flex items-center space-x-4'>
+        <h1 className='text-[35px] uppercase font-bold text-blue-400'>Netflex</h1>
+        <nav className='flex items-center space-x-4'>
+          <a href="" className='text-white'>Home</a>
+          <a href="" className='text-white'>About</a>
+          <a href="" className='text-white'>Contact</a>
+        </nav>
+      </div>
+      <div className='flex items-center space-x-4'>
+        <input type="text" placeholder='Search...' className='outline-none p-4 text-gray-950' />
+        <button className='p-2 text-white bg-blue-500'>Search</button>
+      </div>
     </div>
   )
 }
