8719b6b242c06bd43455f26edfd72e94484ef83a
SangHynh <huynhvansang.020102@gmail.com>
2024-08-01 12:08:33 +0700

    add trailer for movie

--- src/api/apiServices.js
+++ src/api/apiServices.js
@@ -5,7 +5,6 @@ const url2 = 'https://api.themoviedb.org/3/movie/top_rated?language=vi&page=1'
 
 
 const fetchMovies = async () => {
-    console.log(import.meta.env.VITE_API_KEY);
     const response = await fetch(url1,configs);
     const data = await response.json();
     console.log(data);
@@ -13,11 +12,18 @@ const fetchMovies = async () => {
 }
 
 const fetchMoviesRate = async () => {
-    console.log(import.meta.env.VITE_API_KEY);
     const response = await fetch(url2,configs);
     const data = await response.json();
     console.log(data);
     return data;
 }
 
-export {fetchMovies, fetchMoviesRate}
+const fetchMovieTrailer = async (id) => {
+    let url = `https://api.themoviedb.org/3/movie/${id}/videos?language=vi`
+    const response = await fetch(url,configs);
+    const data = await response.json();
+    return data;
+}
+
+
+export {fetchMovies, fetchMoviesRate, fetchMovieTrailer}
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,36 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import ImgTemp from "../assets/temp-1.jpeg";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import Modal from "react-modal";
+import YouTube from "react-youtube";
+import { fetchMovieTrailer } from "../api/apiServices";
 
 const MovieList = ({ title, data }) => {
   console.log(data);
-  
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [trailerKey, setTrailerKey] = useState("");
+
   /* Responsive from carousel lib */
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
-      items: 10
+      items: 10,
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 7
+      items: 7,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 3
+      items: 3,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 2
-    }
+      items: 2,
+    },
+  };
+
+  /* configs for react youtube video */
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  /* handle trailer when click to item*/
+  const handleTrailer = async (id) => {
+    fetchMovieTrailer(id)
+      .then((res) => {
+        setTrailerKey(res.results[0].key);
+        setModalIsOpen(true);
+      })
+      .catch((err) => console.log(err));
   };
 
   /* render item films  */
   const renderItem = (data) => {
     return data.map((item) => (
-      <div key={item.id} className="w-[200px] h-[300px] relative group">
+      <div
+        key={item.id}
+        onClick={() => handleTrailer(item.id)}
+        className="w-[200px] h-[300px] relative group"
+      >
         {/* mask item */}
         <div className="w-full h-full group-hover:scale-105 transition-transform duration-500 ease-in-out cursor-pointer text-white/80">
           <div className="absolute top-0 left-0 w-full h-full bg-black/40"></div>
@@ -41,7 +69,9 @@ const MovieList = ({ title, data }) => {
           />
           {/* name film */}
           <div className="absolute bottom-4 left-4 overflow-hidden">
-            <p className="uppercase text-md text-white/100">{item.title || item.original_title}</p>
+            <p className="uppercase text-md text-white/100">
+              {item.title || item.original_title}
+            </p>
           </div>
         </div>
       </div>
@@ -57,6 +87,30 @@ const MovieList = ({ title, data }) => {
         {/* item film */}
         {data && renderItem(data)}
       </Carousel>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={{
+          overlay: {
+            position: "fixed",
+            zIndex: 9999,
+            backgroundColor: "rgba(0, 0, 0, 0.7)",
+          },
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+            background: "black",
+            padding: "0",
+          },
+        }}
+        contentLabel="Example Modal"
+      >
+        <YouTube videoId={trailerKey} opts={opts} />;
+      </Modal>
     </div>
   );
 };
